5158aa072f084a4d68c20b4539a5895082b92423
Omar Erminy <omrsin@gmail.com>
2012-07-19 02:08:52 -0430

    Commit inicial con configuración

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landingpager::Application.config.secret_token = 'ab1125a446aa67a1f0b2feb4f8e14e781a458883f596aa97d1f09424fc0560b027b5b30ed7394f9cfcaa6bf014b5e0762e54b6943aa46b294c01aba6a1b29d72'
