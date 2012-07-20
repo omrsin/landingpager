3f14e1512ddd3fda033c6550594b1ed745bdde57
Omar Erminy <omrsin@gmail.com>
2012-07-20 19:16:08 -0430

    Cambio de estilo en el Header. Eliminado barra fija superior

--- app/controllers/core_controller.rb
+++ app/controllers/core_controller.rb
@@ -4,7 +4,23 @@ class CoreController < ApplicationController
   
   def generate
   	respond_to do |format|
-  		format.html { send_data params[:html_string], filename: "landingpage.html" }
+  		format.html { send_data generateMarkup(params[:html_string]), 
+  														filename: "landingpage.html" }
   	end
+  end
+  
+  private	
+  
+  def generateMarkup(content)
+  	upper = "<html>\n\t"+
+  						"<head>\n\t\t"+
+  							"<title>Landingpager Site</title>\n\t\t"+
+								"<meta name=\"keywords\" content=\"\">\n\t\t"+
+								"<meta name=\"description\" content=\"\">\n\t"+
+  						"</head>\n\t"+
+  						"<body>\n"
+  	lower = 	"\n\t</body>\n"+
+  					"</html>"
+  	upper + content + lower
   end
 end
